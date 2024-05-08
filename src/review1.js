89356597e4b8878107a0a26b3609113873084000
moonhyunhu <ansgusgn@gmaile.com>
2024-05-08 12:55:11 +0900

    수정

--- src/review1.js
+++ src/review1.js
@@ -1,25 +1,28 @@
-const form = document.querySelector('form');
-const ul = document.querySelector('ul');
-const clrbtn = document.querySelector('#clear');
-const addbtn = document.querySelector('#add');
-const id = document.querySelector('#name');
-const pwd = document.querySelector('#pwd');
-const newreview = document.querySelector('#review');
+//const form = document.querySelector('form');
+const ul = document.querySelector('#review-box');
+//const clrbtn = document.querySelector('#clear');
+const addbtn = document.querySelector('#review-btn');
+const id = document.querySelector('#name-input');
+const pwd = document.querySelector('#pw-input');
+const newreview = document.querySelector('#review-input');
 
 let itemsArray = localStorage.getItem('review') ? JSON.parse(localStorage.getItem('review')) : [];
 
 localStorage.setItem('review', JSON.stringify(itemsArray));
 const data = JSON.parse(localStorage.getItem('review'));
 
-const liMaker = (id, pwd, newreview) => {
+
+
+const liMaker = (id,pwd,newreview) => {
     const li = document.createElement('li');
-    li.innerHTML = `작성자: ${id},  비밀번호: ${pwd}</br>내용: ${newreview}`;
+    ul.innerHTML = `작성자: ${id},  비밀번호: ${pwd}</br>내용: ${newreview}`;
     ul.appendChild(li);
 }
 
 data.forEach(item => {
     liMaker(item);
 });
+
 
 addbtn.addEventListener('click', function (e) {
     e.preventDefault();
@@ -38,10 +41,10 @@ addbtn.addEventListener('click', function (e) {
     liMaker(idValue, pwdValue, revValue);
 });
 
-clrbtn.addEventListener('click', function () {
-    localStorage.clear();
-    while (ul.firstChild) {
-        ul.removeChild(ul.firstChild);
-    }
-});
+//clrbtn.addEventListener('click', function () {
+//    localStorage.clear();
+//    while (ul.firstChild) {
+//        ul.removeChild(ul.firstChild);
+//    }
+//});
 
